a82d587914cc042328f445ae76de79b81f1edac1
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2021-02-05 21:05:48 +0530

    new steps outlines added

--- steps.sh
+++ steps.sh
@@ -9,11 +9,17 @@
 # git clone https://github.com/mrityunjaygr8/bombadil.git
 # bombadil install -c bombadil/bombadil.toml
 # bombadil link
-# mkdir -p .config
+# paru -S ttf-hack ttf-ibm-plex
+## install virtualenvwrapper
 ## install zplug, zsh
 # curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+# zplug install
 ## now our pacman hook has been created, the hook's post run script has been generated
 ## further, the configs have been placed where they need to be
+## install plug from neovim
+# sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+## install tmux package manager, tpm
+# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ## now we can just install the packages
 # sudo pacman -S --needed - < bombadil/hooks/pacman_pkglist.out
 # paru -S --needed - < bombadil/hooks/aur_pkglist.out
