4ef3d1e2c84272c1ad1bcbb2c0787adbc7c8c43e
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2021-02-27 17:07:16 +0530

    unused apps removed from autstart

--- awesome/configuration/autostart.lua
+++ awesome/configuration/autostart.lua
@@ -7,16 +7,16 @@ return {
 
     'picom --config ' .. filesystem.get_configuration_dir() .. 'configuration/picom.conf',
     'nm-applet --indicator', -- wifi
-    'blueberry-tray', -- Bluetooth tray icon
-    'xfce4-power-manager', -- Power manager
+    'mate-power-manager', -- Power manager
     'pasystray', -- PulseAudio Systray Application
     'numlockx on', -- enable numlock
+    'redshift -l  18.55432:73.94159', -- redshift with kharadi co-ordinates
+    'xfce4-clipman', -- clipboard manager
     -- '/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager (alternate directory if the first one is incorrect)
     '/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1  & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
     --'/usr/lib/x86_64-linux-gnu/libexec/polkit-kde-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
     'blueman-tray', -- bluetooth tray
-    'uget-ght', -- uget download manager
-    -- 'dunst', -- notifiaction daemon
+    'uget-gtk', -- uget download manager
 
     -- Add applications that need to be killed between reloads
     -- to avoid multipled instances, inside the awspawn script
