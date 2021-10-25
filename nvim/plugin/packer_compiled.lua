88e533e25e52b675dc213d09181e2be00eb4c89d
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2021-10-25 22:34:48 +0530

    new packer added

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -44,8 +44,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/mrityunjaygr8/.cache/nvim/packer_hererocks/2.0.5/share/lua/5.1/?.lua;/home/mrityunjaygr8/.cache/nvim/packer_hererocks/2.0.5/share/lua/5.1/?/init.lua;/home/mrityunjaygr8/.cache/nvim/packer_hererocks/2.0.5/lib/luarocks/rocks-5.1/?.lua;/home/mrityunjaygr8/.cache/nvim/packer_hererocks/2.0.5/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/mrityunjaygr8/.cache/nvim/packer_hererocks/2.0.5/lib/lua/5.1/?.so"
+local package_path_str = "/home/mgr8/.cache/nvim/packer_hererocks/2.0.5/share/lua/5.1/?.lua;/home/mgr8/.cache/nvim/packer_hererocks/2.0.5/share/lua/5.1/?/init.lua;/home/mgr8/.cache/nvim/packer_hererocks/2.0.5/lib/luarocks/rocks-5.1/?.lua;/home/mgr8/.cache/nvim/packer_hererocks/2.0.5/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/mgr8/.cache/nvim/packer_hererocks/2.0.5/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -69,85 +69,132 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
+  ["Comment.nvim"] = {
+    config = { "\27LJ\1\0025\0\0\2\0\3\0\0064\0\0\0%\1\1\0>\0\2\0027\0\2\0>\0\1\1G\0\1\0\nsetup\fComment\frequire\0" },
+    loaded = true,
+    path = "/home/mgr8/.local/share/nvim/site/pack/packer/start/Comment.nvim"
+  },
   LuaSnip = {
     loaded = true,
-    path = "/home/mrityunjaygr8/.local/share/nvim/site/pack/packer/start/LuaSnip"
+    path = "/home/mgr8/.local/share/nvim/site/pack/packer/start/LuaSnip"
+  },
+  ["barbar.nvim"] = {
+    loaded = true,
+    path = "/home/mgr8/.local/share/nvim/site/pack/packer/start/barbar.nvim"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/home/mrityunjaygr8/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp"
+    path = "/home/mgr8/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp"
   },
   cmp_luasnip = {
     loaded = true,
-    path = "/home/mrityunjaygr8/.local/share/nvim/site/pack/packer/start/cmp_luasnip"
+    path = "/home/mgr8/.local/share/nvim/site/pack/packer/start/cmp_luasnip"
+  },
+  ["dashboard-nvim"] = {
+    loaded = true,
+    path = "/home/mgr8/.local/share/nvim/site/pack/packer/start/dashboard-nvim"
   },
   ["gitsigns.nvim"] = {
     loaded = true,
-    path = "/home/mrityunjaygr8/.local/share/nvim/site/pack/packer/start/gitsigns.nvim"
+    path = "/home/mgr8/.local/share/nvim/site/pack/packer/start/gitsigns.nvim"
   },
   ["indent-blankline.nvim"] = {
     loaded = true,
-    path = "/home/mrityunjaygr8/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim"
+    path = "/home/mgr8/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim"
   },
   ["lightline.vim"] = {
     loaded = true,
-    path = "/home/mrityunjaygr8/.local/share/nvim/site/pack/packer/start/lightline.vim"
+    path = "/home/mgr8/.local/share/nvim/site/pack/packer/start/lightline.vim"
+  },
+  ["nvim-autopairs"] = {
+    loaded = true,
+    path = "/home/mgr8/.local/share/nvim/site/pack/packer/start/nvim-autopairs"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/home/mrityunjaygr8/.local/share/nvim/site/pack/packer/start/nvim-cmp"
+    path = "/home/mgr8/.local/share/nvim/site/pack/packer/start/nvim-cmp"
   },
   ["nvim-lsp-installer"] = {
     loaded = true,
-    path = "/home/mrityunjaygr8/.local/share/nvim/site/pack/packer/start/nvim-lsp-installer"
+    path = "/home/mgr8/.local/share/nvim/site/pack/packer/start/nvim-lsp-installer"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/home/mrityunjaygr8/.local/share/nvim/site/pack/packer/start/nvim-lspconfig"
+    path = "/home/mgr8/.local/share/nvim/site/pack/packer/start/nvim-lspconfig"
+  },
+  ["nvim-tree.lua"] = {
+    config = { "\27LJ\1\2;\0\0\2\0\3\0\a4\0\0\0%\1\1\0>\0\2\0027\0\2\0002\1\0\0>\0\2\1G\0\1\0\nsetup\14nvim-tree\frequire\0" },
+    loaded = true,
+    path = "/home/mgr8/.local/share/nvim/site/pack/packer/start/nvim-tree.lua"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/home/mrityunjaygr8/.local/share/nvim/site/pack/packer/start/nvim-treesitter"
+    path = "/home/mgr8/.local/share/nvim/site/pack/packer/start/nvim-treesitter"
   },
   ["nvim-treesitter-textobjects"] = {
     loaded = true,
-    path = "/home/mrityunjaygr8/.local/share/nvim/site/pack/packer/start/nvim-treesitter-textobjects"
+    path = "/home/mgr8/.local/share/nvim/site/pack/packer/start/nvim-treesitter-textobjects"
+  },
+  ["nvim-web-devicons"] = {
+    loaded = true,
+    path = "/home/mgr8/.local/share/nvim/site/pack/packer/start/nvim-web-devicons"
   },
   ["onedark.vim"] = {
     loaded = true,
-    path = "/home/mrityunjaygr8/.local/share/nvim/site/pack/packer/start/onedark.vim"
+    path = "/home/mgr8/.local/share/nvim/site/pack/packer/start/onedark.vim"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/home/mrityunjaygr8/.local/share/nvim/site/pack/packer/start/packer.nvim"
+    path = "/home/mgr8/.local/share/nvim/site/pack/packer/start/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/home/mrityunjaygr8/.local/share/nvim/site/pack/packer/start/plenary.nvim"
+    path = "/home/mgr8/.local/share/nvim/site/pack/packer/start/plenary.nvim"
+  },
+  ["structlog.nvim"] = {
+    loaded = true,
+    path = "/home/mgr8/.local/share/nvim/site/pack/packer/start/structlog.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/home/mrityunjaygr8/.local/share/nvim/site/pack/packer/start/telescope.nvim"
+    path = "/home/mgr8/.local/share/nvim/site/pack/packer/start/telescope.nvim"
   },
   ["vim-commentary"] = {
     loaded = true,
-    path = "/home/mrityunjaygr8/.local/share/nvim/site/pack/packer/start/vim-commentary"
+    path = "/home/mgr8/.local/share/nvim/site/pack/packer/start/vim-commentary"
   },
   ["vim-fugitive"] = {
     loaded = true,
-    path = "/home/mrityunjaygr8/.local/share/nvim/site/pack/packer/start/vim-fugitive"
+    path = "/home/mgr8/.local/share/nvim/site/pack/packer/start/vim-fugitive"
   },
   ["vim-gutentags"] = {
     loaded = true,
-    path = "/home/mrityunjaygr8/.local/share/nvim/site/pack/packer/start/vim-gutentags"
+    path = "/home/mgr8/.local/share/nvim/site/pack/packer/start/vim-gutentags"
   },
   ["vim-rhubarb"] = {
     loaded = true,
-    path = "/home/mrityunjaygr8/.local/share/nvim/site/pack/packer/start/vim-rhubarb"
+    path = "/home/mgr8/.local/share/nvim/site/pack/packer/start/vim-rhubarb"
+  },
+  ["which-key.nvim"] = {
+    config = { "\27LJ\1\2;\0\0\2\0\3\0\a4\0\0\0%\1\1\0>\0\2\0027\0\2\0002\1\0\0>\0\2\1G\0\1\0\nsetup\14which-key\frequire\0" },
+    loaded = true,
+    path = "/home/mgr8/.local/share/nvim/site/pack/packer/start/which-key.nvim"
   }
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: Comment.nvim
+time([[Config for Comment.nvim]], true)
+try_loadstring("\27LJ\1\0025\0\0\2\0\3\0\0064\0\0\0%\1\1\0>\0\2\0027\0\2\0>\0\1\1G\0\1\0\nsetup\fComment\frequire\0", "config", "Comment.nvim")
+time([[Config for Comment.nvim]], false)
+-- Config for: nvim-tree.lua
+time([[Config for nvim-tree.lua]], true)
+try_loadstring("\27LJ\1\2;\0\0\2\0\3\0\a4\0\0\0%\1\1\0>\0\2\0027\0\2\0002\1\0\0>\0\2\1G\0\1\0\nsetup\14nvim-tree\frequire\0", "config", "nvim-tree.lua")
+time([[Config for nvim-tree.lua]], false)
+-- Config for: which-key.nvim
+time([[Config for which-key.nvim]], true)
+try_loadstring("\27LJ\1\2;\0\0\2\0\3\0\a4\0\0\0%\1\1\0>\0\2\0027\0\2\0002\1\0\0>\0\2\1G\0\1\0\nsetup\14which-key\frequire\0", "config", "which-key.nvim")
+time([[Config for which-key.nvim]], false)
 if should_profile then save_profiles() end
 
 end)
