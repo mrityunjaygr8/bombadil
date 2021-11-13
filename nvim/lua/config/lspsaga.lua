a66456de037ebe293d756de99c67663e11c39d0e
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2021-11-13 15:58:27 +0530

    diagnostic header icon commented

--- nvim/lua/config/lspsaga.lua
+++ nvim/lua/config/lspsaga.lua
@@ -3,7 +3,7 @@ local saga = require("lspsaga")
 saga.init_lsp_saga({
   code_action_icon = " ",
   definition_preview_icon = "  ",
-  diagnostic_header_icon = "   ",
+  -- diagnostic_header_icon = "   ",
   error_sign = " ",
   finder_definition_icon = "  ",
   finder_reference_icon = "  ",
