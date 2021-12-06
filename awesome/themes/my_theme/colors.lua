e38f59df841d27de5191448f90dbc8b90e4651e8
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2021-12-06 19:57:48 +0530

    awesome colors are now hardcoded

---
+++ awesome/themes/my_theme/colors.lua
@@ -0,0 +1,31 @@
+local pywal = {
+
+
+  wallpaper = "/home/mgr8/Pictures/slide/wallhaven-399115.jpg",
+
+  special = {
+    background = "#05142E",
+    foreground = "#e6c6e0",
+    cursor = "#e6c6e0",
+  },
+  colors = {
+    color0 = "#05142E",
+    color1 = "#9D61A3",
+    color2 = "#D073B5",
+    color3 = "#4B8FD9",
+    color4 = "#E694A8",
+    color5 = "#CEAAB5",
+    color6 = "#A8A0D4",
+    color7 = "#e6c6e0",
+    color8 = "#a18a9c",
+    color9 = "#9D61A3",
+    color10 = "#D073B5",
+    color11 = "#4B8FD9",
+    color12 = "#E694A8",
+    color13 = "#CEAAB5",
+    color14 = "#A8A0D4",
+    color15 = "#e6c6e0",
+  }
+}
+return pywal
+
