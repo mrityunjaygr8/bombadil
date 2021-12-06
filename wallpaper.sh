17d9270c6784c00d43d283d2756402eb4cf322b3
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2021-12-06 19:45:19 +0530

    pyfox call commented out

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,4 +1,4 @@
 wal -i $HOME/Pictures/slide
 feh --bg-scale "$(< "${HOME}/.cache/wal/wal")"
 # gsettings set org.gnome.desktop.background picture-uri file:///"$(< "${HOME}/.cache/wal/wal")"
-$HOME/commands/Pywalfox/daemon/pywalfox.py update
+# $HOME/commands/Pywalfox/daemon/pywalfox.py update
