410bf0109e38fb5b0af56b33e305180689eb6cba
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2021-12-29 18:40:03 +0530

    formatting done; scrolloff set to 999

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -6,17 +6,17 @@ vim.g.shiftwidth = 2
 vim.g.tabstop = 2
 vim.g.hidden = true
 
-vim.wo.signcolumn="yes:2"
+vim.wo.signcolumn = "yes:2"
 
-vim.cmd('set clipboard=unnamedplus')
+vim.cmd("set clipboard=unnamedplus")
+vim.cmd("set scrolloff=999")
 
 --Map blankline
-vim.g.indent_blankline_char = '┊'
-vim.g.indent_blankline_filetype_exclude = { 'help', 'packer' }
-vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile' }
-vim.g.indent_blankline_char_highlight = 'LineNr'
+vim.g.indent_blankline_char = "┊"
+vim.g.indent_blankline_filetype_exclude = { "help", "packer" }
+vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
+vim.g.indent_blankline_char_highlight = "LineNr"
 vim.g.indent_blankline_show_trailing_blankline_indent = false
-
 
 --Make line numbers default
 vim.wo.number = true
@@ -24,7 +24,6 @@ vim.wo.relativenumber = true
 
 --Do not save when switching buffers (note: this is now a default on master)
 vim.o.hidden = true
-
 
 --Enable break indent
 vim.o.breakindent = true
@@ -45,9 +44,8 @@ vim.o.termguicolors = true
 ----Lua:
 --vim.g.material_style = "deep ocean"
 
-
 -- set gutentags cache dir
-vim.g.gutentags_cache_dir = '~/.cache'
+vim.g.gutentags_cache_dir = "~/.cache"
 
 -- highlight on yank
 vim.api.nvim_exec(
@@ -61,17 +59,19 @@ vim.api.nvim_exec(
 )
 
 -- Illuminate config
-vim.api.nvim_command [[ hi def link LspReferenceText CursorLine ]]
-vim.api.nvim_command [[ hi def link LspReferenceWrite CursorLine ]]
-vim.api.nvim_command [[ hi def link LspReferenceRead CursorLine ]]
+vim.api.nvim_command([[ hi def link LspReferenceText CursorLine ]])
+vim.api.nvim_command([[ hi def link LspReferenceWrite CursorLine ]])
+vim.api.nvim_command([[ hi def link LspReferenceRead CursorLine ]])
 
 -- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
+vim.o.completeopt = "menuone,noselect"
 
-vim.api.nvim_exec([[
+vim.api.nvim_exec(
+  [[
 augroup FormatAutogroup
   autocmd!
   autocmd BufWritePost *.js,*.rs,*.lua,*.py,*.tsx,*.ts FormatWrite
 augroup END
-]], true)
-
+]],
+  true
+)
