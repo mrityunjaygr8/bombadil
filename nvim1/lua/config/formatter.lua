43e74080277646a66c17959945dbdc946787ce02
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-04-27 16:21:02 +0530

    old nvim config name changed

---
+++ nvim1/lua/config/formatter.lua
@@ -0,0 +1,83 @@
+require("formatter").setup({
+  filetype = {
+    python = {
+      -- Configuration for psf/black
+      function()
+        return {
+          exe = "black", -- this should be available on your $PATH
+          args = { "-" },
+          stdin = true,
+        }
+      end,
+    },
+    lua = {
+      function()
+        return {
+          exe = "stylua",
+          args = {
+            "--config-path " .. "~/.config/stylua/stylua.toml",
+            "-",
+          },
+          stdin = true,
+        }
+      end,
+    },
+    typescriptreact = {
+      -- prettier
+      function()
+        return {
+          exe = "prettier",
+          args = { "--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)) },
+          stdin = true,
+        }
+      end,
+    },
+    typescript = {
+      -- prettier
+      function()
+        return {
+          exe = "prettier",
+          args = { "--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)) },
+          stdin = true,
+        }
+      end,
+    },
+    javascript = {
+      -- prettier
+      function()
+        return {
+          exe = "prettier",
+          args = { "--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)) },
+          stdin = true,
+        }
+      end,
+    },
+    terraform = {
+      function()
+        return {
+          exe = "terraform",
+          args = { "fmt", "-" },
+          stdin = true,
+        }
+      end,
+    },
+    go = {
+      function()
+        return {
+          exe = "goimports",
+          args = { "-w", vim.api.nvim_buf_get_name(0) },
+          stdin = false,
+        }
+      end,
+    },
+    rust = {
+      rustfmt = function()
+        return {
+          exe = "rustfmt",
+          args = { "--emit=stdout" },
+          stdin = true,
+        }
+      end,
+    },
+  },
+})
---
+++ nvim1/lua/plugins.lua
@@ -0,0 +1,89 @@
+local fn = vim.fn
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  })
+end
+
+local use = require("packer").use
+return require("packer").startup(function(use)
+  -- My plugins here
+  -- use 'foo1/bar1.nvim'
+  -- use 'foo2/bar2.nvim'
+  use("wbthomason/packer.nvim")
+  use("lewis6991/impatient.nvim")
+  use("tpope/vim-commentary") -- "gc" to comment visual regions/lines
+  -- use 'ludovicchabant/vim-gutentags' -- Automatic tags management
+  use({
+    "nvim-telescope/telescope.nvim",
+    requires = { "nvim-lua/plenary.nvim" },
+    cmd = "Telescope",
+    module = "telescope",
+  })
+  use({
+    "nvim-lualine/lualine.nvim",
+    requires = { "kyazdani42/nvim-web-devicons", opt = true },
+  })
+
+  -- Themes start
+  use("EdenEast/nightfox.nvim")
+  use({ "catppuccin/nvim", as = "catppuccin" })
+
+  -- Themes end
+  use("nathom/filetype.nvim")
+  use({ "lewis6991/gitsigns.nvim", requires = { "nvim-lua/plenary.nvim" } })
+  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+  use("lukas-reineke/indent-blankline.nvim")
+  use({ "nvim-treesitter/nvim-treesitter" })
+  -- Additional textobjects for treesitter
+  use("nvim-treesitter/nvim-treesitter-textobjects")
+  use("neovim/nvim-lspconfig") -- Collection of configurations for built-in LSP client
+  use("williamboman/nvim-lsp-installer")
+  use("hrsh7th/nvim-cmp") -- Autocompletion plugin
+  use("hrsh7th/cmp-nvim-lsp")
+  use("saadparwaiz1/cmp_luasnip")
+  use("L3MON4D3/LuaSnip") -- Snippets plugin
+
+  use("tpope/vim-sleuth")
+  use({
+    "kyazdani42/nvim-tree.lua",
+    requires = "kyazdani42/nvim-web-devicons",
+  })
+  use("dstein64/vim-startuptime")
+  use("kdheepak/lazygit.nvim")
+  use("numtostr/FTerm.nvim")
+  use("chaoren/vim-wordmotion")
+  use({
+    "romgrk/barbar.nvim",
+    requires = { "kyazdani42/nvim-web-devicons" },
+  })
+  use("RRethy/vim-illuminate")
+  use("christoomey/vim-system-copy")
+  use({ "tami5/lspsaga.nvim" })
+  use({ "tpope/vim-surround" })
+  use("mhartington/formatter.nvim")
+  use({
+    "danymat/neogen",
+    config = function()
+      require("neogen").setup({
+        enabled = true,
+        languages = {
+          typescriptreact = require("neogen.configurations.typescript"),
+        },
+      })
+    end,
+    requires = "nvim-treesitter/nvim-treesitter",
+  })
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require("packer").sync()
+  end
+end)
