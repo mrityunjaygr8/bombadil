ff7b1f9c23e7295239c23eaa4584820b8025f4fc
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-08-15 16:25:34 +0530

    updated comment.nvim to use new api

--- nvim/lua/user/whichkey.lua
+++ nvim/lua/user/whichkey.lua
@@ -79,7 +79,7 @@ local opts = {
 }
 
 local mappings = {
-  ["/"] = { '<cmd>lua require("Comment.api").toggle_current_linewise()<CR>', "Comment" },
+  ["/"] = { '<cmd>lua require("Comment.api").toggle.linewise.current()<CR>', "Comment" },
   ["b"] = {
     "<cmd>lua require('telescope.builtin').buffers(require('telescope.themes').get_dropdown{previewer = false})<cr>",
     "Buffers",
@@ -165,7 +165,7 @@ local vopts = {
   nowait = true, -- use `nowait` when creating keymaps
 }
 local vmappings = {
-  ["/"] = { '<ESC><CMD>lua require("Comment.api").toggle_linewise_op(vim.fn.visualmode())<CR>', "Comment" },
+  ["/"] = { '<ESC><CMD>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>', "Comment" },
 }
 
 which_key.setup(setup)
