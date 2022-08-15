6075b661379c7f0b884134a8b2fe254609e8ec08
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-08-15 16:12:54 +0530

    old confs removed

--- nvim_old/after/plugin/autocmds.lua
+++
@@ -1,8 +0,0 @@
-local api = vim.api
-
--- windows to close with "q"
-api.nvim_create_autocmd(
-	"FileType",
-	{ pattern = { "help", "startuptime", "qf", "lspinfo" }, command = [[nnoremap <buffer><silent> q :close<CR>]] }
-)
-api.nvim_create_autocmd("FileType", { pattern = "man", command = [[nnoremap <buffer><silent> q :quit<CR>]] })
--- nvim_old/lua/config/snip/init.lua
+++
@@ -1,9 +0,0 @@
--- Load snippets
-require("luasnip/loaders/from_vscode").lazy_load()
-require("luasnip.loaders.from_snipmate").lazy_load()
-
--- Load custom javascript
-require("luasnip.loaders.from_vscode").lazy_load { paths = { "./snippets/typescript" } }
-require("luasnip.loaders.from_vscode").lazy_load { paths = { "./snippets/python" } }
-
-ls.filetype_extend("all", { "_" })
